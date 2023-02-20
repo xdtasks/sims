abf82f083754bd27261c97a7adca31074aac3ccc
xdtasks <125533504+xdtasks@users.noreply.github.com>
2023-02-20 15:47:57 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect('mongodb+srv://naveen:naveen123@cluster0.6tk87vt.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect('mongodb+srv://xd666:naveen123@cluster.gbcmjjt.mongodb.net/?retryWrites=true&w=majority'
 
 // mongoose.connect('mongodb://127.0.0.1:27017/student-internship-api'
 );
